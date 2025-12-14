38ed4760e25be2b95a3f160138ab1604ef5b16ea
Karan <karan.l.k123321@gmail.com>
2025-12-14 22:17:01 +0530

    Server Entry point

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8000;
+
+//Routes will be here...
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
